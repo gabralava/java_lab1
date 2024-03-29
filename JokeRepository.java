94ab770a633621cfbb2bd0c5cfbc1eeacb3f23ff
Abralava Georgii <69796158+gabralava@users.noreply.github.com>
2024-03-29 13:15:46 +0300

    Delete JokeRepository.java

--- JokeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.anekdotbot.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.anekdotbot.project.demo.model.JokeData;
-
-public interface JokeRepository extends JpaRepository<JokeData, Integer>{
-    
-}
-
