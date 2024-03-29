19b5bf0657cbdb7c2d2f8e15229cb5932e90fec4
Abralava Georgii <69796158+gabralava@users.noreply.github.com>
2024-03-29 13:17:37 +0300

    Update JokeController.java

--- Lab1/demo/src/main/java/ru/anekdotbot/project/demo/controller/JokeController.java
+++ Lab1/demo/src/main/java/ru/anekdotbot/project/demo/controller/JokeController.java
@@ -1,8 +1,8 @@
 package ru.anekdotbot.project.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,54 +12,55 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import ru.anekdotbot.project.demo.model.JokeData;
 import ru.anekdotbot.project.demo.repository.JokeRepository;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/jokes")
 public class JokeController {
 
     @Autowired
     private JokeRepository jokeRepository;
 
-    @GetMapping("/jokes")
-    public List<JokeData> getJokes() {
-        return jokeRepository.getAllJokes();
+    @GetMapping
+    public List<JokeData> getAllJokes() {
+        return jokeRepository.findAll();
     }
 
-    @GetMapping("/jokes/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<JokeData> getJokeById(@PathVariable int id) {
-        Optional<JokeData> joke = jokeRepository.getJokeById(id);
+        Optional<JokeData> joke = jokeRepository.findById(id);
         return joke.map(j -> new ResponseEntity<>(j, HttpStatus.OK))
-                   .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping("/jokes")
-    public ResponseEntity<Void> addJoke(@RequestBody JokeData joke) {
-        jokeRepository.addJoke(joke);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<JokeData> addJoke(@RequestBody JokeData joke) {
+        JokeData savedJoke = jokeRepository.save(joke);
+        return new ResponseEntity<>(savedJoke, HttpStatus.CREATED);
     }
 
-
-    @PutMapping("jokes/{id}")
-    public ResponseEntity<Void> updateJoke(@PathVariable int id, @RequestBody JokeData updatedJoke) {
-        updatedJoke.setId(id);
-        if (jokeRepository.updateJoke(updatedJoke)) {
-            return new ResponseEntity<>(HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<JokeData> updateJoke(@PathVariable int id, @RequestBody JokeData updatedJoke) {
+        if (jokeRepository.existsById(id)) {
+            updatedJoke.setId(id);
+            JokeData savedJoke = jokeRepository.save(updatedJoke);
+            return new ResponseEntity<>(savedJoke, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-    } 
+    }
 
-    @DeleteMapping("jokes/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteJoke(@PathVariable int id) {
-        if (jokeRepository.deleteJoke(id)) {
+        if (jokeRepository.existsById(id)) {
+            jokeRepository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    
 }
